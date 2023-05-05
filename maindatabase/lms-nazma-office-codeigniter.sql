96b682193b600fd204830f86ca5c4921dee187ef
Muhammad Afifudin <muhafifudin2306@gmail.com>
2023-05-05 14:13:40 +0700

    SQL DB

--- maindatabase/lms-nazma-office-codeigniter.sql
+++ maindatabase/lms-nazma-office-codeigniter.sql
@@ -1,317 +1,152 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Mar 19, 2023 at 10:56 AM
--- Server version: 5.7.33
--- PHP Version: 7.4.19
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               8.0.30 - MySQL Community Server - GPL
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.1.0.6537
+-- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Database: `cms-nazma-office-codeigniter`
---
+-- Dumping structure for table lms-nazma-office-codeigniter.categories
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- --------------------------------------------------------
+-- Data exporting was unselected.
 
---
--- Table structure for table `admin`
---
+-- Dumping structure for table lms-nazma-office-codeigniter.courses
+CREATE TABLE IF NOT EXISTS `courses` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `cover` text,
+  `rating` decimal(20,6) DEFAULT NULL,
+  `participant` int DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `instructor` varchar(255) DEFAULT NULL,
+  `summary` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `admin` (
-  `id` int(11) NOT NULL,
-  `username` varchar(200) NOT NULL,
-  `password` varchar(200) NOT NULL,
-  `role` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+-- Data exporting was unselected.
 
---
--- Dumping data for table `admin`
---
+-- Dumping structure for table lms-nazma-office-codeigniter.course_has_category
+CREATE TABLE IF NOT EXISTS `course_has_category` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `id_course` bigint unsigned DEFAULT NULL,
+  `id_category` bigint unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_course_has_category_courses` (`id_course`),
+  KEY `FK_course_has_category_categories` (`id_category`),
+  CONSTRAINT `FK_course_has_category_categories` FOREIGN KEY (`id_category`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_course_has_category_courses` FOREIGN KEY (`id_course`) REFERENCES `courses` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-INSERT INTO `admin` (`id`, `username`, `password`, `role`) VALUES
-(1, 'nazmaoffice', '$2y$10$vD04zjip4eKE9AAqvoDuTe21.Jx/427E5f9CfbXPavB0pKQfF/kHa', 'A');
+-- Data exporting was unselected.
 
--- --------------------------------------------------------
+-- Dumping structure for table lms-nazma-office-codeigniter.members
+CREATE TABLE IF NOT EXISTS `members` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `summary` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `job` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `address` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `district` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `region` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `pos-code` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `province` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `instagram` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `linkedin` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `gmail` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Table structure for table `blog`
---
+-- Data exporting was unselected.
 
-CREATE TABLE `blog` (
-  `id` int(11) NOT NULL,
-  `photo` varchar(300) NOT NULL,
-  `date` date NOT NULL,
-  `slug` varchar(300) NOT NULL,
-  `title` varchar(300) NOT NULL,
-  `content` text NOT NULL,
-  `tag` varchar(300) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+-- Dumping structure for table lms-nazma-office-codeigniter.member_has_tag
+CREATE TABLE IF NOT EXISTS `member_has_tag` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `id_member` bigint unsigned DEFAULT NULL,
+  `id_tag` bigint unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_member_has_tag_members` (`id_member`),
+  KEY `FK_member_has_tag_tags` (`id_tag`),
+  CONSTRAINT `FK_member_has_tag_members` FOREIGN KEY (`id_member`) REFERENCES `members` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_member_has_tag_tags` FOREIGN KEY (`id_tag`) REFERENCES `tags` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Dumping data for table `blog`
---
+-- Data exporting was unselected.
 
-INSERT INTO `blog` (`id`, `photo`, `date`, `slug`, `title`, `content`, `tag`) VALUES
-(5, 'Web_capture_6-3-2023_135317_phppasswordhash.com.jpeg', '2023-03-18', 'test-test-test', 'test test test ', '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed viverra ipsum nunc aliquet. Vestibulum lectus mauris ultrices eros in cursus. Auctor elit sed vulputate mi sit amet mauris commodo quis. Vulputate mi sit amet mauris commodo. Dolor magna eget est lorem ipsum dolor. Cum sociis natoque penatibus et. Urna id volutpat lacus laoreet non curabitur. Urna duis convallis convallis tellus id. Ultricies mi eget mauris pharetra et ultrices neque ornare aenean.</p>\r\n\r\n<p>Id venenatis a condimentum vitae sapien pellentesque habitant morbi tristique. Vulputate dignissim suspendisse in est ante in nibh mauris cursus. Orci phasellus egestas tellus rutrum tellus. At volutpat diam ut venenatis tellus in metus vulputate eu. Ut etiam sit amet nisl purus in. Pharetra massa massa ultricies mi quis hendrerit dolor magna eget. Sem integer vitae justo eget. Ipsum consequat nisl vel pretium lectus quam id leo. Magna sit amet purus gravida quis. Pulvinar pellentesque habitant morbi tristique senectus et. Enim lobortis scelerisque fermentum dui. Vitae auctor eu augue ut lectus. In nibh mauris cursus mattis molestie a. At in tellus integer feugiat scelerisque. Pulvinar neque laoreet suspendisse interdum consectetur libero. Amet justo donec enim diam vulputate ut pharetra. Porttitor leo a diam sollicitudin tempor id eu. In eu mi bibendum neque egestas congue. Risus in hendrerit gravida rutrum quisque non. Pretium nibh ipsum consequat nisl vel pretium lectus.</p>\r\n\r\n<p>Ut ornare lectus sit amet est placerat in egestas. Bibendum enim facilisis gravida neque convallis a cras. Praesent tristique magna sit amet purus gravida quis blandit. Nisi lacus sed viverra tellus in hac habitasse. Proin sagittis nisl rhoncus mattis. Amet consectetur adipiscing elit duis tristique. Faucibus pulvinar elementum integer enim neque volutpat ac tincidunt vitae. Lectus arcu bibendum at varius vel pharetra vel turpis. A condimentum vitae sapien pellentesque habitant morbi tristique. Tellus id interdum velit laoreet. Duis at tellus at urna condimentum. Magna etiam tempor orci eu lobortis. Aliquet enim tortor at auctor urna nunc id cursus metus. Enim blandit volutpat maecenas volutpat blandit aliquam etiam.</p>', 'lorem | ipsum | dolor | amet '),
-(6, 'vuejs14.jpg', '2023-03-19', 'test', 'test', 'test', 'test'),
-(7, 'vuejs15.jpg', '2023-03-17', 'test', 'test', 'test', 'test');
+-- Dumping structure for table lms-nazma-office-codeigniter.playlists
+CREATE TABLE IF NOT EXISTS `playlists` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- --------------------------------------------------------
+-- Data exporting was unselected.
 
---
--- Table structure for table `certifiedtrainers`
---
+-- Dumping structure for table lms-nazma-office-codeigniter.progress
+CREATE TABLE IF NOT EXISTS `progress` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `id_playlist` bigint unsigned DEFAULT NULL,
+  `id_video` bigint unsigned DEFAULT NULL,
+  `id_course` bigint unsigned DEFAULT NULL,
+  `id_user` bigint unsigned DEFAULT NULL,
+  `status` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_playlist_has_video_courses` (`id_course`),
+  KEY `FK_classes_playlists` (`id_playlist`),
+  KEY `FK_classes_videos` (`id_video`),
+  KEY `FK_progress_users` (`id_user`),
+  CONSTRAINT `FK_classes_playlists` FOREIGN KEY (`id_playlist`) REFERENCES `playlists` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_classes_videos` FOREIGN KEY (`id_video`) REFERENCES `videos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_playlist_has_video_courses` FOREIGN KEY (`id_course`) REFERENCES `courses` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_progress_users` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `certifiedtrainers` (
-  `id` int(11) NOT NULL,
-  `photo` varchar(300) NOT NULL,
-  `name` varchar(300) NOT NULL,
-  `departement` varchar(300) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+-- Data exporting was unselected.
 
---
--- Dumping data for table `certifiedtrainers`
---
+-- Dumping structure for table lms-nazma-office-codeigniter.tags
+CREATE TABLE IF NOT EXISTS `tags` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `name` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-INSERT INTO `certifiedtrainers` (`id`, `photo`, `name`, `departement`) VALUES
-(1, 'profil18.jpg', 'Muhammad Afifudin', 'Staff IT'),
-(2, 'profil19.jpg', 'Muhammad Afifudin', 'Staff IT'),
-(3, 'profil110.jpg', 'Muhammad Afifudin', 'Staff IT'),
-(4, 'profil111.jpg', 'Muhammad Afifudin', 'Staff IT');
+-- Data exporting was unselected.
 
--- --------------------------------------------------------
+-- Dumping structure for table lms-nazma-office-codeigniter.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Table structure for table `creativecrew`
---
+-- Data exporting was unselected.
 
-CREATE TABLE `creativecrew` (
-  `id` int(11) NOT NULL,
-  `photo` varchar(300) NOT NULL,
-  `name` varchar(300) NOT NULL,
-  `departement` varchar(300) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+-- Dumping structure for table lms-nazma-office-codeigniter.videos
+CREATE TABLE IF NOT EXISTS `videos` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `link` text,
+  `duration` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Dumping data for table `creativecrew`
---
+-- Data exporting was unselected.
 
-INSERT INTO `creativecrew` (`id`, `photo`, `name`, `departement`) VALUES
-(1, 'profil18.jpg', 'Muhammad Afifudin', 'Staff IT'),
-(2, 'profil19.jpg', 'Muhammad Afifudin', 'Staff IT'),
-(3, 'profil110.jpg', 'Muhammad Afifudin', 'Staff IT'),
-(4, 'profil111.jpg', 'Muhammad Afifudin', 'Staff IT');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `event`
---
-
-CREATE TABLE `event` (
-  `id` int(11) NOT NULL,
-  `photo` varchar(300) NOT NULL,
-  `title` varchar(300) NOT NULL,
-  `time` varchar(300) NOT NULL,
-  `location` varchar(300) NOT NULL,
-  `price` varchar(300) NOT NULL,
-  `contributor` varchar(300) NOT NULL,
-  `link` varchar(300) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `event`
---
-
-INSERT INTO `event` (`id`, `photo`, `title`, `time`, `location`, `price`, `contributor`, `link`) VALUES
-(4, 'design_grafis_5_11.jpg', 'Pelatihan Pembuatan Logo dan Company Profile', 'Kamis, 23 Februari 2023, 08.00 WIB', 'Kabupaten Sleman, Daerah Istimewa Yogyakarta', 'Start at 2000k', 'NaZMa Office', 'https://linktr.ee/nazma_office'),
-(5, 'design_grafis_5_12.jpg', 'Pelatihan Pembuatan Logo dan Company Profile', 'Kamis, 23 Februari 2023, 08.00 WIB', 'Kabupaten Sleman, Daerah Istimewa Yogyakarta', 'Start at 2000k', 'NaZMa Office', 'https://linktr.ee/nazma_office'),
-(6, 'design_grafis_5_13.jpg', 'Pelatihan Pembuatan Logo dan Company Profile', 'Kamis, 23 Februari 2023, 08.00 WIB', 'Kabupaten Sleman, Daerah Istimewa Yogyakarta', 'Start at 2000k', 'NaZMa Office', 'https://linktr.ee/nazma_office'),
-(7, 'design_grafis_5_14.jpg', 'Pelatihan Pembuatan Logo dan Company Profile', 'Kamis, 23 Februari 2023, 08.00 WIB', 'Kabupaten Sleman, Daerah Istimewa Yogyakarta', 'Start at 2000k', 'NaZMa Office', 'https://linktr.ee/nazma_office');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `partnership`
---
-
-CREATE TABLE `partnership` (
-  `id` int(11) NOT NULL,
-  `photo` varchar(300) NOT NULL,
-  `name` varchar(300) NOT NULL,
-  `departement` varchar(300) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `partnership`
---
-
-INSERT INTO `partnership` (`id`, `photo`, `name`, `departement`) VALUES
-(1, 'logoukmindonesia1.jpg', 'UMKM INDONESIA', 'Usaha'),
-(2, 'ukm_indonesia_21.jpg', 'UMKM INDONESIA', 'Usaha'),
-(3, 'ukm_indonesia_11.jpg', 'UMKM INDONESIA', 'Usaha'),
-(4, 'umkmnaikkelas1.jpg', 'UMKM INDONESIA', 'Usaha'),
-(5, 'logo_amikom1.jpg', 'UNIVERSITAS AMIKOM YOGYAKARTA', 'Pendidikan');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `project`
---
-
-CREATE TABLE `project` (
-  `id` int(11) NOT NULL,
-  `photo` varchar(300) NOT NULL,
-  `title` varchar(300) NOT NULL,
-  `description` text NOT NULL,
-  `date` date NOT NULL,
-  `location` varchar(300) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `project`
---
-
-INSERT INTO `project` (`id`, `photo`, `title`, `description`, `date`, `location`) VALUES
-(1, 'design_grafis_2.jpg', 'Bimtek Pemasaran Digital Marketing', 'Tim NaZMa mengisi salah satu pelatihan bagi UMKM di wilayah Yogyakarta yang dihadiri 100 peserta yang menghadirkan materi digital marketing guna memaksimalkan pemasaran di era digital', '2023-02-28', 'Kabupaten Sleman, Yogyakarta'),
-(2, 'design_grafis_21.jpg', 'Bimtek Pemasaran Digital Marketing', 'Tim NaZMa mengisi salah satu pelatihan bagi UMKM di wilayah Yogyakarta yang dihadiri 100 peserta yang menghadirkan materi digital marketing guna memaksimalkan pemasaran di era digital', '2023-02-28', 'Kabupaten Sleman, Yogyakarta');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `testimony`
---
-
-CREATE TABLE `testimony` (
-  `id` int(11) NOT NULL,
-  `photo` varchar(300) NOT NULL,
-  `name` varchar(300) NOT NULL,
-  `description` varchar(300) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `testimony`
---
-
-INSERT INTO `testimony` (`id`, `photo`, `name`, `description`) VALUES
-(1, 'profil1.jpg', 'Muhammad Afifudin', ' \"Tim IT NaZMa sangat responsif dan selalu siap membantu kami dengan pertanyaan atau masalah yang kami miliki. Kami sangat merekomendasikan layanan IT mereka\"'),
-(2, 'profil11.jpg', 'Muhammad Afifudin', ' \"Tim IT NaZMa sangat responsif dan selalu siap membantu kami dengan pertanyaan atau masalah yang kami miliki. Kami sangat merekomendasikan layanan IT mereka\"');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `blog`
---
-ALTER TABLE `blog`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `certifiedtrainers`
---
-ALTER TABLE `certifiedtrainers`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `creativecrew`
---
-ALTER TABLE `creativecrew`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `event`
---
-ALTER TABLE `event`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `partnership`
---
-ALTER TABLE `partnership`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `project`
---
-ALTER TABLE `project`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `testimony`
---
-ALTER TABLE `testimony`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `admin`
---
-ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `blog`
---
-ALTER TABLE `blog`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT for table `certifiedtrainers`
---
-ALTER TABLE `certifiedtrainers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `creativecrew`
---
-ALTER TABLE `creativecrew`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `event`
---
-ALTER TABLE `event`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT for table `partnership`
---
-ALTER TABLE `partnership`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `project`
---
-ALTER TABLE `project`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `testimony`
---
-ALTER TABLE `testimony`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-COMMIT;
-
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
