a67323853c2b2b11129595774b2fa8b00546a394
Muhammad Afifudin <muhafifudin2306@gmail.com>
2023-05-09 13:32:12 +0700

    new_database

--- maindatabase/lms-nazma-office-codeigniter.sql
+++ maindatabase/lms-nazma-office-codeigniter.sql
@@ -1,59 +1,73 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               8.0.30 - MySQL Community Server - GPL
--- Server OS:                    Win64
--- HeidiSQL Version:             12.1.0.6537
--- --------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Waktu pembuatan: 09 Bulan Mei 2023 pada 06.31
+-- Versi server: 8.0.30
+-- Versi PHP: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- Dumping structure for table lms-nazma-office-codeigniter.categories
-CREATE TABLE IF NOT EXISTS `categories` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+--
+-- Database: `lms-nazma-office-codeigniter`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` bigint UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Data exporting was unselected.
+-- --------------------------------------------------------
 
--- Dumping structure for table lms-nazma-office-codeigniter.courses
-CREATE TABLE IF NOT EXISTS `courses` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+--
+-- Struktur dari tabel `courses`
+--
+
+CREATE TABLE `courses` (
+  `id` bigint UNSIGNED NOT NULL,
   `cover` text,
   `rating` decimal(20,6) DEFAULT NULL,
   `participant` int DEFAULT NULL,
   `title` varchar(255) DEFAULT NULL,
   `instructor` varchar(255) DEFAULT NULL,
-  `summary` text,
-  PRIMARY KEY (`id`)
+  `summary` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Data exporting was unselected.
+-- --------------------------------------------------------
 
--- Dumping structure for table lms-nazma-office-codeigniter.course_has_category
-CREATE TABLE IF NOT EXISTS `course_has_category` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `id_course` bigint unsigned DEFAULT NULL,
-  `id_category` bigint unsigned DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_course_has_category_courses` (`id_course`),
-  KEY `FK_course_has_category_categories` (`id_category`),
-  CONSTRAINT `FK_course_has_category_categories` FOREIGN KEY (`id_category`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `FK_course_has_category_courses` FOREIGN KEY (`id_course`) REFERENCES `courses` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+--
+-- Struktur dari tabel `course_has_category`
+--
+
+CREATE TABLE `course_has_category` (
+  `id` bigint UNSIGNED NOT NULL,
+  `id_course` bigint UNSIGNED DEFAULT NULL,
+  `id_category` bigint UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Data exporting was unselected.
+-- --------------------------------------------------------
 
--- Dumping structure for table lms-nazma-office-codeigniter.members
-CREATE TABLE IF NOT EXISTS `members` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+--
+-- Struktur dari tabel `members`
+--
+
+CREATE TABLE `members` (
+  `id` bigint UNSIGNED NOT NULL,
   `summary` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
   `job` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `address` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
@@ -63,90 +77,302 @@ CREATE TABLE IF NOT EXISTS `members` (
   `province` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `instagram` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `linkedin` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `gmail` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `gmail` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Data exporting was unselected.
+-- --------------------------------------------------------
 
--- Dumping structure for table lms-nazma-office-codeigniter.member_has_tag
-CREATE TABLE IF NOT EXISTS `member_has_tag` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `id_member` bigint unsigned DEFAULT NULL,
-  `id_tag` bigint unsigned DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_member_has_tag_members` (`id_member`),
-  KEY `FK_member_has_tag_tags` (`id_tag`),
-  CONSTRAINT `FK_member_has_tag_members` FOREIGN KEY (`id_member`) REFERENCES `members` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `FK_member_has_tag_tags` FOREIGN KEY (`id_tag`) REFERENCES `tags` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+--
+-- Struktur dari tabel `member_has_tag`
+--
+
+CREATE TABLE `member_has_tag` (
+  `id` bigint UNSIGNED NOT NULL,
+  `id_member` bigint UNSIGNED DEFAULT NULL,
+  `id_tag` bigint UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Data exporting was unselected.
+-- --------------------------------------------------------
 
--- Dumping structure for table lms-nazma-office-codeigniter.playlists
-CREATE TABLE IF NOT EXISTS `playlists` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+--
+-- Struktur dari tabel `playlists`
+--
+
+CREATE TABLE `playlists` (
+  `id` bigint UNSIGNED NOT NULL,
+  `name` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Data exporting was unselected.
+-- --------------------------------------------------------
 
--- Dumping structure for table lms-nazma-office-codeigniter.progress
-CREATE TABLE IF NOT EXISTS `progress` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `id_playlist` bigint unsigned DEFAULT NULL,
-  `id_video` bigint unsigned DEFAULT NULL,
-  `id_course` bigint unsigned DEFAULT NULL,
-  `id_user` bigint unsigned DEFAULT NULL,
-  `status` int DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_playlist_has_video_courses` (`id_course`),
-  KEY `FK_classes_playlists` (`id_playlist`),
-  KEY `FK_classes_videos` (`id_video`),
-  KEY `FK_progress_users` (`id_user`),
-  CONSTRAINT `FK_classes_playlists` FOREIGN KEY (`id_playlist`) REFERENCES `playlists` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `FK_classes_videos` FOREIGN KEY (`id_video`) REFERENCES `videos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `FK_playlist_has_video_courses` FOREIGN KEY (`id_course`) REFERENCES `courses` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `FK_progress_users` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+--
+-- Struktur dari tabel `progress`
+--
+
+CREATE TABLE `progress` (
+  `id` bigint UNSIGNED NOT NULL,
+  `id_playlist` bigint UNSIGNED DEFAULT NULL,
+  `id_video` bigint UNSIGNED DEFAULT NULL,
+  `id_course` bigint UNSIGNED DEFAULT NULL,
+  `id_user` bigint UNSIGNED DEFAULT NULL,
+  `status` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Data exporting was unselected.
+-- --------------------------------------------------------
 
--- Dumping structure for table lms-nazma-office-codeigniter.tags
-CREATE TABLE IF NOT EXISTS `tags` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `name` bigint DEFAULT NULL,
-  PRIMARY KEY (`id`)
+--
+-- Struktur dari tabel `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` bigint UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Data exporting was unselected.
+--
+-- Dumping data untuk tabel `roles`
+--
 
--- Dumping structure for table lms-nazma-office-codeigniter.users
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+INSERT INTO `roles` (`id`, `name`) VALUES
+(1, 'Super Admin'),
+(2, 'Instructor'),
+(3, 'Member');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tags`
+--
+
+CREATE TABLE `tags` (
+  `id` bigint UNSIGNED NOT NULL,
+  `name` bigint DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint UNSIGNED NOT NULL,
   `email` varchar(255) NOT NULL,
   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `password` varchar(255) NOT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- Data exporting was unselected.
-
--- Dumping structure for table lms-nazma-office-codeigniter.videos
-CREATE TABLE IF NOT EXISTS `videos` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `link` text,
-  `duration` int DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `id_role` bigint UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Data exporting was unselected.
+--
+-- Dumping data untuk tabel `users`
+--
 
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+INSERT INTO `users` (`id`, `email`, `name`, `password`, `created_at`, `updated_at`, `id_role`) VALUES
+(10, 'muhafifudin66@gmail.com', 'Muhammad Afifudin', '$2y$10$LvUn3mQZXhqfNKuzUHtHKOKje5bB7F7ACXphpgO4wWMN4sHEs1JW6', '2023-05-09 01:21:48', '2023-05-09 01:21:48', 1),
+(21, 'anisa66@gmail.com', 'Anisa Mahda Habsari', '$2y$10$sVq5z2Tr774mJJ2UiVK7OOWbN1fxPuoVAv2Z/fD76hLh.CNCC2uHC', '2023-05-09 01:52:28', '2023-05-09 01:52:28', 3),
+(22, 'jafowikab@mailinator.com', 'Jaden Rich', '$2y$10$nlbtKajrO8KTX3w0KchejugPrhp/AttzIwPF47MkSaFKyWhNK9ae6', '2023-05-09 02:50:49', '2023-05-09 02:50:49', 2),
+(23, 'pasatoger@mailinator.com', 'Briar Bowman', '$2y$10$JNEdV2iPeukU0oskkMXqtuKFC7L7.BwXkzsK.avsNHUM3RE1NzRQC', '2023-05-09 03:00:48', '2023-05-09 03:00:48', 2),
+(24, 'lova@mailinator.com', 'on', '$2y$10$TKS37SY2XlQRoTvzKrr4gu6dtmuZKnN08i/Nufp9mrVk5uWe4.Axe', '2023-05-09 03:00:39', '2023-05-09 03:00:39', 3),
+(25, 'kabytoxi@mailinator.com', 'Afifudi', '$2y$10$nRlq8fWjW.JtMiJsMxqQ4eO06w0K7Sw6p67fLg4nEdgM9n.qnLD.C', '2023-05-09 05:58:01', '2023-05-09 05:58:01', 3),
+(26, 'muhafifudin2306@gmail.com', 'Afifudin', '$2y$10$G1rjibJg/.8AfN5aiy57Gu0oYQT2fXxvI.GM/rx7mXMvFI1W3vr8C', '2023-05-09 06:18:49', '2023-05-09 06:18:49', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `videos`
+--
+
+CREATE TABLE `videos` (
+  `id` bigint UNSIGNED NOT NULL,
+  `link` text,
+  `duration` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `courses`
+--
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `course_has_category`
+--
+ALTER TABLE `course_has_category`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_course_has_category_courses` (`id_course`),
+  ADD KEY `FK_course_has_category_categories` (`id_category`);
+
+--
+-- Indeks untuk tabel `members`
+--
+ALTER TABLE `members`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `member_has_tag`
+--
+ALTER TABLE `member_has_tag`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_member_has_tag_members` (`id_member`),
+  ADD KEY `FK_member_has_tag_tags` (`id_tag`);
+
+--
+-- Indeks untuk tabel `playlists`
+--
+ALTER TABLE `playlists`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `progress`
+--
+ALTER TABLE `progress`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_playlist_has_video_courses` (`id_course`),
+  ADD KEY `FK_classes_playlists` (`id_playlist`),
+  ADD KEY `FK_classes_videos` (`id_video`),
+  ADD KEY `FK_progress_users` (`id_user`);
+
+--
+-- Indeks untuk tabel `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `tags`
+--
+ALTER TABLE `tags`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_users_roles` (`id_role`);
+
+--
+-- Indeks untuk tabel `videos`
+--
+ALTER TABLE `videos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `courses`
+--
+ALTER TABLE `courses`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `course_has_category`
+--
+ALTER TABLE `course_has_category`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `members`
+--
+ALTER TABLE `members`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `member_has_tag`
+--
+ALTER TABLE `member_has_tag`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `playlists`
+--
+ALTER TABLE `playlists`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `progress`
+--
+ALTER TABLE `progress`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `tags`
+--
+ALTER TABLE `tags`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT untuk tabel `videos`
+--
+ALTER TABLE `videos`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `course_has_category`
+--
+ALTER TABLE `course_has_category`
+  ADD CONSTRAINT `FK_course_has_category_categories` FOREIGN KEY (`id_category`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_course_has_category_courses` FOREIGN KEY (`id_course`) REFERENCES `courses` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `member_has_tag`
+--
+ALTER TABLE `member_has_tag`
+  ADD CONSTRAINT `FK_member_has_tag_members` FOREIGN KEY (`id_member`) REFERENCES `members` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_member_has_tag_tags` FOREIGN KEY (`id_tag`) REFERENCES `tags` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `progress`
+--
+ALTER TABLE `progress`
+  ADD CONSTRAINT `FK_classes_playlists` FOREIGN KEY (`id_playlist`) REFERENCES `playlists` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_classes_videos` FOREIGN KEY (`id_video`) REFERENCES `videos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_playlist_has_video_courses` FOREIGN KEY (`id_course`) REFERENCES `courses` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_progress_users` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `FK_users_roles` FOREIGN KEY (`id_role`) REFERENCES `roles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
