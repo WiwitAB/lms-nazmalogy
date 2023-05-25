df7b75429b5566e6020883ec87255ed4c9c6fed2
Muhammad Afifudin <muhafifudin2306@gmail.com>
2023-05-25 09:53:18 +0700

    Fixed support front page

--- application/views/support.php
+++ application/views/support.php
@@ -1,101 +1,73 @@
 <section id="support" class="bg-light-primary">
-    <div class="container">
+    <div class="container col-10" data-aos="fade-up" data-aos-duration="700">
         <div class="pt-5 my-5">
-            <div class="container-1">
-                <div class="row">
-                    <div class="col ">
-                        <h1>Tanyakan masalah kepada kami</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-                    </div>
-                    <div class="col-md-auto">
-                        <img src="<?= base_url('assets/images/support/support-icon-1.png') ?>" alt="support-icon-1" width="80">
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                    <div class="container-1">
-                        <div class="row">
-                            <div class="col">
-                                <h1>Basis Pengetahuan</h1>
-                                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula. parturient </p>
-                            </div>
-                            <div class="col-md-auto">
-                                <img src="<?= base_url('assets/images/support/support-icon-2.png') ?>" alt="support-icon-2" width="80">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="container-1">
-                        <div class="row">
-                            <div class="col">
-                                <h1>Bantuan Lengkap</h1>
-                                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula. parturient </p>
-                            </div>
-                            <div class="col-md-auto">
-                                <img src="<?= base_url('assets/images/support/support-icon-3.png') ?>" alt="support-icon-3" width="80">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
             <!-- ======= pertanyaan cepat di jawab ====== -->
-            <h1 class="questions-title">Pertanyaan cepat dijawab</h1>
+            <h1 class="questions-title text-legend pt-5">FAQ (Frequently Asked Question)</h1>
             <div class="row">
-                <div class="col-8">
+                <div class="col-md-8 mb-4">
                     <div class="accordion accordion-flush" id="accordionFlushExample">
                         <div class="accordion-item m-2">
                             <h2 class="accordion-header" id="flush-headingOne">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                                    Bagaimana memakai layanan di NaZMalogy?
+                                <button class="fw-bold accordion-button collapsed border" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                                    Apa itu NaZMalogy?
                                 </button>
                             </h2>
                             <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
                                 <div class="accordion-body">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                    NaZMaLogy adalah Learning Management System (LMS) yang dibangun oleh NaZMa Office yang memudahkan pengguna dalam pembelajaran jarak jauh. Dengan platform media pembelajaran berbasis website yang menyediakan video yang inovatif dan interaktif diharapkan memudahkan pengguna mewujudkan impiannya. #BersamaNaZMaLogyBerinovasiMewujudkanMimpi
+
                                 </div>
                             </div>
                         </div>
                         <div class="accordion-item m-2">
                             <h2 class="accordion-header" id="flush-headingTwo">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                                <button class="fw-bold accordion-button collapsed border" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
                                     Apa kelebihan NaZMalogy?
                                 </button>
                             </h2>
                             <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
                                 <div class="accordion-body">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                    <strong>Pembelajaran efektif</strong> : platform LMS yang menyediakan metode pembelajaran jarak jauh yang efektif, yaitu video pembelajaran, mentoring, quiz, dan Feedback Instruktur.
+                                    <br><br>
+                                    <strong>PWA Support dan Interaktif</strong> : platform LMS menyediakan kemudahan akses cepat dan ringan di berbagai perangkat.
+                                    <br><br>
+                                    <strong>Pelacak progress</strong> : pemantau kemajuan pembelajaran mandiri pengguna.
+                                    <br><br>
+                                    <strong>Forum Group Discussion</strong> : platform LMS menyediakan tempat sebagai forum pengguna dengan pengguna lain serta mentor untuk saling bertanya, menjawab, serta berkolaborasi.
                                 </div>
                             </div>
                         </div>
                         <div class="accordion-item m-2">
                             <h2 class="accordion-header" id="flush-headingThree">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-                                    Bagaimana berlangganan di NaZMalogy?
+                                <button class="fw-bold accordion-button collapsed border" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                                    Bagaimana saya menghubungi call center ketika mendapatkan masalah ?
                                 </button>
                             </h2>
                             <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
                                 <div class="accordion-body">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                    Anda dapat ke laman <strong>Bantuan</strong> dan klik tombol <strong>Ajukan Pertanyaan</strong> atau <strong>Kontak Kami</strong>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col">
-                    <div class="container-2">
-                        <h1>Navigasi Bantuan</h1>
+                <div class="col-md-4 mb-5">
+                    <div class="bg-white p-3">
+                        <h4 class="text-legend">Navigasi Bantuan</h4>
                         <hr>
-                        <div class="d-grid gap-2">
-                            <button class="btn btn-ask" type="button">
-                                <img src="<?= base_url('assets/images/support/support-ask.png') ?>" alt="support-icon-1" width="25">
-                                Mengajukan Pertanyaan
-                            </button>
-                            <button class="btn btn-contact" type="button">
-                                <img src="<?= base_url('assets/images/support/support-contact.png') ?>" alt="support-icon-2" width="25">
-                                Kontak Kami
-                            </button>
+                        <div class="d-flex flex-column gap-2 py-1">
+                            <a href="https://wa.link/6u8kcx" target="_blank">
+                                <button class="btn btn-primary bg-first text-white py-3 w-100" type="button">
+                                    <img src="<?= base_url('assets/images/support/support-ask.png') ?>" alt="support-icon-1" width="25">
+                                    Ajukan Pertanyaan
+                                </button>
+                            </a>
+                            <a href="https://linktr.ee/nazma_office" target="_blank">
+                                <button class="btn btn-warning bg-orange text-white py-3 w-100" type="button">
+                                    <img src="<?= base_url('assets/images/support/support-contact.png') ?>" alt="support-icon-2" width="25">
+                                    Kontak Kami
+                                </button>
+                            </a>
                         </div>
                     </div>
                 </div>
